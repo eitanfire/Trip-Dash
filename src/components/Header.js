3ef53f7e2b51b09f935b8f601b908127e79fc281
Eitan Fire <eitanfire@gmail.com>
2023-03-31 19:23:48 -0600

    style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Col, Container, Navbar, NavbarBrand } from "reactstrap";
+import { Col, Row, Container, Navbar, NavbarBrand } from "reactstrap";
 import logo from '../assets/img/trip-dash-logo.png'
 import "../App.css";
 
@@ -6,14 +6,16 @@ const Header = () => {
     return (
       <Navbar className="NavBar" dark color="primary" sticky="top" expand="md">
         <Container>
-          <Col>
-            <NavbarBrand href="/">
-              <img className="logo" src={logo} alt="trip dash logo" />
-            </NavbarBrand>
-          </Col>
-          <Col>
-            <h1 className="text-center">☕️ Coffee for a Cause 🇬🇹</h1>
-          </Col>
+          <Row>
+            <Col>
+              <NavbarBrand href="/">
+                <img className="logo" src={logo} alt="trip dash logo" />
+              </NavbarBrand>
+            </Col>
+            <Col>
+              <h1>☕️ Coffee for a Cause 🇬🇹</h1>
+            </Col>
+          </Row>
         </Container>
       </Navbar>
     );
