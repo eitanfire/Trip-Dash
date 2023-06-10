1656776768c1794dde368249ba22d2ff58bf63bf
Eitan Fire <eitanfire@gmail.com>
2023-06-09 23:56:34 -0600

    format Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,26 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 const Header = () => {
-    return (
-      <Navbar className="NavBar" dark color="primary" sticky="top" expand="md">
-        <Container>
-          <Row>
-            <Col>
-              <NavbarBrand href="/">
-                <h1 id="header-title" className="col col-ms-8">
-                  ☕️ Coffee for a Cause 🇬🇹
-                </h1>
-              </NavbarBrand>
-            </Col>
-            <Col className="order-link">
-              <Link to="order">
-                <h3>Order</h3>
-              </Link>
-            </Col>
-          </Row>
-        </Container>
-      </Navbar>
-    );
-}
+  return (
+    <Navbar className="NavBar" dark color="primary" sticky="top" expand="md">
+      <Container>
+        <Row>
+          <Col>
+            <NavbarBrand href="/">
+              <h1 id="header-title" className="col col-ms-8">
+                ☕️ Coffee for a Cause 🇬🇹
+              </h1>
+            </NavbarBrand>
+          </Col>
+          <Col className="order-link">
+            <Link to="order">
+              <h3>Order</h3>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
